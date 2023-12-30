8084f078f662970d523b5ff097144d0d99e36c53
Josh Lin <linynjosh@gmail.com>
2023-12-30 00:26:12 -0800

    Setting up the tests

--- test_json_parsor/test_json_parsor.py
+++ test_json_parsor/test_json_parsor.py
@@ -4,17 +4,54 @@ from json_parsor.json_parsor import parse
 step1_valid = {}
 step1_invalid = None
 
-step2_valid = None
-step2_invalid = None
-step2_valid2 = None
-step2_invalid2 = None
+step2_valid = {"key": "value"}
+step2_invalid = {"key": "value",}
+step2_valid2 = {
+  "key": "value",
+  "key2": "value"
+}
+step2_invalid2 = {
+  "key": "value",
+  key2: "value"
+}
 
-step3_valid = None
-step3_invalid = None
+step3_valid = {
+  "key1": true,
+  "key2": false,
+  "key3": null,
+  "key4": "value",
+  "key5": 101
+}
+step3_invalid = {
+  "key1": true,
+  "key2": False,
+  "key3": null,
+  "key4": "value",
+  "key5": 101
+}
 
-step4_valid = None
-step4_invalid = None
-step4_valid2 = None
+step4_valid = {
+  "key": "value",
+  "key-n": 101,
+  "key-o": {},
+  "key-l": []
+}
+step4_invalid = {
+  "key": "value",
+  "key-n": 101,
+  "key-o": {
+    "inner key": "inner value"
+  },
+  "key-l": ['list value']
+}
+step4_valid2 = {
+  "key": "value",
+  "key-n": 101,
+  "key-o": {
+    "inner key": "inner value"
+  },
+  "key-l": ["list value"]
+}
 
 def test_step1_valid():
     assert parse(step1_valid) == True
